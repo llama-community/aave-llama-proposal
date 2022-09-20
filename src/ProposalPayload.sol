e20886e09d0ef6868a81e85627526a2a42b4e7ac
Rajath Alex <rajathalex@gmail.com>
2022-09-20 16:51:58 -0400

    Initial Proposal Payload

--- src/ProposalPayload.sol
+++ src/ProposalPayload.sol
@@ -1,28 +1,59 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
-import {ILendingPool} from "./external/aave/ILendingPool.sol";
+import {IAaveEcosystemReserveController} from "./external/aave/IAaveEcosystemReserveController.sol";
+import {IStreamable} from "./external/aave/IStreamable.sol";
 
 /**
- * @title <TITLE>
- * @author <AUTHOR>
- * @notice <DESCRIPTION>
- * Governance Forum Post:
+ * @title Llama <> AAVE Proposal
+ * @author Llama
+ * @notice Payload to execute the Llama <> AAVE Proposal
+ * Governance Forum Post: https://governance.aave.com/t/updated-proposal-llama-aave/9924
  * Snapshot:
  */
 contract ProposalPayload {
-    /*///////////////////////////////////////////////////////////////
-                               CONSTANTS
-    //////////////////////////////////////////////////////////////*/
+    /********************************
+     *   CONSTANTS AND IMMUTABLES   *
+     ********************************/
 
-    /// @notice EXAMPLE CONSTANT.
-    /// @notice AAVE V2 lending pool.
-    ILendingPool private constant lendingPool = ILendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
+    IAaveEcosystemReserveController public constant AAVE_ECOSYSTEM_RESERVE_CONTROLLER =
+        IAaveEcosystemReserveController(0x3d569673dAa0575c936c7c67c4E6AedA69CC630C);
 
-    /// @notice EXAMPLE CONSTANT.
-    /// @notice usdc token.
-    address private constant usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    IStreamable public constant ECOSYSTEM_RESERVE_V2_IMPL = IStreamable(0x1aa435ed226014407Fa6b889e9d06c02B1a12AF3);
+
+    address public constant AAVE_MAINNET_RESERVE_FACTOR = 0x464C71f6c2F760DdA6093dCB91C24c39e5d6e18c;
+    address public constant AAVE_ECOSYSTEM_RESERVE = 0x25F2226B597E8F9514B3F68F00f494cF4f286491;
+    address public constant LLAMA_TREASURY = 0xA519a7cE7B24333055781133B13532AEabfAC81b;
+
+    address public constant AUSDC_TOKEN = 0xBcca60bB61934080951369a648Fb03DF4F96263C;
+    address public constant AAVE_TOKEN = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
+
+    // 500,000 aUSDC = $0.5 Million
+    uint256 public constant AUSDC_UPFRONT_AMOUNT = 500000e6; // 500,000 aUSDC
+
+    // TODO: Figure out why we need the additional amount for streaming
+    // ~700,000 aUSDC (A bit more for the streaming requirements) = $0.7 million
+    uint256 public constant AUSDC_STREAM_AMOUNT = 700000e6;
+    // ~3'480 AAVE (A bit more for the streaming requirements) = $0.3 Million using 30 day TWAP on day of proposal
+    uint256 public constant AAVE_STREAM_AMOUNT = 3480e18;
+    // 12 months of 30 days
+    uint256 public constant STREAMS_DURATION = 360 days; // 12 months of 30 days
+
+    /*****************
+     *   FUNCTIONS   *
+     *****************/
 
     /// @notice The AAVE governance executor calls this function to implement the proposal.
-    function execute() external {}
+    function execute() external {
+        // Transfer of the upfront payment: $0.5 million in aUSDC
+        AAVE_ECOSYSTEM_RESERVE_CONTROLLER.transfer(
+            AAVE_MAINNET_RESERVE_FACTOR,
+            AUSDC_TOKEN,
+            LLAMA_TREASURY,
+            AUSDC_UPFRONT_AMOUNT
+        );
+
+        // TODO: Implement streaming of aUSDC and AAVE over 12 months
+        // TODO: Figure out if and how to implement the stream cancel ability after 6 months here
+    }
 }
